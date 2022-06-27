a860d401d4c2866a98e61cd555b7cffcaf210ad8
zelymm <dkdlwkrz@naver.com>
2022-06-28 02:25:41 +0900

    Add a what page

---
+++ src/pages/spacetest.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SpacePage() {
+  return (
+    <main>
+      <title>What's wrong</title>
+      <h1>What's wrong?</h1>
+    </main>
+  );
+}
+
+export default SpacePage;
